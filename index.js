6c820c07200f7229a1318f06714eb7424a10923c
nilesh-r <nileshsahu8674@gmail.com>
2025-03-16 14:17:28 +0530

    change

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ app.post("/transcribe", upload.single("file"), async (req, res) => {
     }
   });
 // Start server
-const PORT = process.env.PORT ||
+const PORT = process.env.PORT ||4000;
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 
